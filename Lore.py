b79361b3c3f47e4b8a6b7ddb3a2f1b195cb70c21
Hamis <96818360+GrepalyBoti@users.noreply.github.com>
2025-02-19 16:53:03 +0100

    Add files via upload

---
+++ Lore.py
@@ -0,0 +1,6 @@
+def get_intro_lore():
+    return (
+        "In the ancient lands of Eldoria, a great darkness has begun to rise. "
+        "Legends speak of heroes who will stand against the growing tide of evil. "
+        "You are one such adventurer, setting forth on a journey that will shape the fate of the world."
+    )
